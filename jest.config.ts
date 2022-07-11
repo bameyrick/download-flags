a959d3b842bce2c45dceaa15a0ac0977529d6a3b
Ben Meyrick <bameyrick@gmail.com>
2022-07-11 10:20:35 +0100

    Add tsransformIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import { InitialOptionsTsJest } from 'ts-jest';
 
+const esModules = [].join('|');
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`],
 } as InitialOptionsTsJest;
